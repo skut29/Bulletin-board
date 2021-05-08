f000bda3438be5d2d3cb136352fab3206aac7be0
Sarah Abdulaleem <amyourgirl16@gmail.com>
2021-05-08 19:33:23 -0400

    changing file structure

---
+++ backend/controllers/note.js
@@ -0,0 +1,31 @@
+const Note = require('../models/Note');
+
+
+const getAllNotes = (req, res, next) => {
+    res.json({note: "GET new note"});
+};
+
+const newNote = (req, res, next) => {
+    const newNote = new Note({
+        note: req.body.note
+    })
+    newNote.save((err, data)=>{
+        if(err) return res.json({Error: err});
+        return res.json(data);
+    })
+};
+
+const editNote = (req, res, next) => {
+    res.json({note: "PUT new note"});
+};
+
+const deleteNote = (req, res, next) => {
+    res.json({note: "DELETE new note"});
+};
+ 
+module.exports = {
+    getAllNotes,
+    newNote,
+    editNote,
+    deleteNote
+};
--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,60 @@
 const express = require("express");
-const router = express.Router();
+const routes = require('./routes/note');
 bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require("dotenv").config();
-const Note = require("./models/Note");
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use('/', routes);
+// app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true  })
-.then(() =>  console.log('connection succesful'))
-.catch((err) => console.error(err));;
 
-router.get("/", async (req, res) => {
-  const note = new Note({note: "ffxv"});
-
-  try {
-    await note.save();
-    res.send("inserted data");
-  } catch (err) {
-    console.log(err)
-  }
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  client.close();
 });
 
-router.post("/notes", (req, res) => {
-  const newNote = new Note(req.body);
-  newNote
-  .save()
-  .then((newNote) => {
-    res.json(newNote);
-  })
-  .catch((err) => {
-    res.status(500).send(err.message);
-  });
-});
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  Note.findById(id, (err, note) => {
-    res.json(note)
-  });
-});
+// router.get("/", async (req, res) => {
+//   const note = new Note({note: "ffxv"});
 
-// app.put()
+//   try {
+//     await note.save();
+//     res.send("inserted data");
+//   } catch (err) {
+//     console.log(err)
+//   }
+// });
 
-// app.delete()
+// router.post("/notes", (req, res) => {
+//   const newNote = new Note(req.body);
+//   newNote
+//   .save()
+//   .then((newNote) => {
+//     res.json(newNote);
+//   })
+//   .catch((err) => {
+//     res.status(500).send(err.message);
+//   });
+// });
+
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   Note.findById(id, (err, note) => {
+//     res.json(note)
+//   });
+// });
+
+// // app.put()
+
+// // app.delete()
 
 
 
