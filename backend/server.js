53e99459fa60ce9c0204c7f953c71043359fe13f
Sarah Abdulaleem <amyourgirl16@gmail.com>
2021-05-08 19:43:04 -0400

    cleaning server.js

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-// app.use('/', routes);
-// app.use(bodyParser.json());
 
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = process.env.MONGODB_URI;
 
@@ -20,41 +17,6 @@ client.connect(err => {
   const collection = client.db("test").collection("devices");
   client.close();
 });
-
-
-// router.get("/", async (req, res) => {
-//   const note = new Note({note: "ffxv"});
-
-//   try {
-//     await note.save();
-//     res.send("inserted data");
-//   } catch (err) {
-//     console.log(err)
-//   }
-// });
-
-// router.post("/notes", (req, res) => {
-//   const newNote = new Note(req.body);
-//   newNote
-//   .save()
-//   .then((newNote) => {
-//     res.json(newNote);
-//   })
-//   .catch((err) => {
-//     res.status(500).send(err.message);
-//   });
-// });
-
-// router.get("/:id", (req, res) => {
-//   const id = req.params.id;
-//   Note.findById(id, (err, note) => {
-//     res.json(note)
-//   });
-// });
-
-// // app.put()
-
-// // app.delete()
 
 
 
