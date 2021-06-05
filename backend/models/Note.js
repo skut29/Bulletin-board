cbd8144e87bd19547e5139c2971a51d7d6cdb1b7
Sarah Abdulaleem <amyourgirl16@gmail.com>
2021-06-05 13:19:08 -0400

    removing backend folder

--- backend/models/Note.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const NoteSchema = new mongoose.Schema({
-    note: {type: String, required: true}
-});
-
-const Note = mongoose.model('Note', NoteSchema);
-module.exports = Note;
